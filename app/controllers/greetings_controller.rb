0103ebd91dac05ea2a4d9900eb6d0dc0e0990f43
rbhatt199924 <rbhatt199924@gmail.com>
2023-05-02 13:58:41 +0530

    create greeting entity

---
+++ app/controllers/greetings_controller.rb
@@ -0,0 +1,29 @@
+class GreetingsController < ApplicationController
+  before_action :set_greeting, only: %i[ show ]
+
+  # GET /greetings
+  def index
+    @greetings = Greeting.all
+    @greetings = @greetings.map do |greeting|
+      greeting.greeting
+    end
+    @greetings = @greetings.sample
+    render json: {greeting: @greetings}
+  end
+
+  # GET /greetings/1
+  def show
+    render json: @greeting
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_greeting
+      @greeting = Greeting.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def greeting_params
+      params.require(:greeting).permit(:greeting)
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(greeting: "Hello World!")
+Greeting.create(greeting: "Hola Mundo!")
+Greeting.create(greeting: "Bonjour le monde!")
+Greeting.create(greeting: "Hallo Welt!")
+Greeting.create(greeting: "Ciao mondo!")
+Greetings.create(greeting: "Hej världen!")
