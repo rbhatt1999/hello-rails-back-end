7c7250eef733f0c1108e489c2e0d70fa6ae5be5d
rbhatt199924 <rbhatt199924@gmail.com>
2023-05-02 16:50:18 +0530

    add cors

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -6,6 +6,7 @@ class GreetingsController < ApplicationController
     @greetings = Greeting.all
     @greetings = @greetings.map(&:greeting)
     @greetings = @greetings.sample
+    sleep 1
     render json: { greeting: @greetings }
   end
 
